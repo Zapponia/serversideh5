277bce221404a5746cb532c81e010b160acea0eb
mjni.skp <mjni.skp@edu.mercantec.dk>
2021-11-17 00:23:44 +0100

    if else statements placeholder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { read } = require("fs");
 var http = require("http");
 var mysql = require("mysql");
 
@@ -10,10 +11,30 @@ var con = mysql.createConnection({
 
 var server = http.createServer(function(req, res) {
     con.connect(function(err) {
-        con.query("SELECT * FROM users", function(err,data) {
-            res.write(JSON.stringify(data));
-            res.end();
-        });
+        if(req.method === 'GET'){
+            con.query("SELECT * FROM users", function(err,data) {
+                res.write(JSON.stringify(data));
+                res.end();
+            }
+        )} else if(req.method === 'POST'){
+            con.query("INSERT INTO users (Username, Password) VALUES ('test', 'test')", function(err,data){
+                res.write(JSON.stringify(data));
+                res.end();
+            }
+        )} else if(req.method === 'PUT'){
+            con.query("UPDATE users SET Username = 'test123' WHERE Username = 'test'", function(err,data){
+                res.write(JSON.stringify(data));
+                res.end();
+            }
+        )} else if(req.method === 'DELETE'){
+            con.query("DELETE FROM users WHERE Username = 'test'", function(err,data){
+                res.write(JSON.stringify(data));
+                res.end();
+            }
+        )} else{
+            res.write(404)
+            res.end(JSON.stringify({ message: 'Route Not Found'}))
+        }
     });
 });
 
