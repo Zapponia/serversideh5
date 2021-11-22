6f73cbdec8081b601d827b9dc229c3f8d4ffd53c
Marcus Nielsen <mni@mjolner.dk>
2021-11-22 10:49:22 +0100

    made parameters for new sql statemenst

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var server = http.createServer(function(req, res) {
                 });
                 break;
                 case '/user/editUser':
-                    con.query("UPDATE users SET Username = 'test123' WHERE Username = 'test'", function(err,data) {
+                    con.query("UPDATE users SET Username = ? WHERE Username = ?",['test1234', 'test'], function(err,data) {
                         res.write(JSON.stringify(data));
                         res.end();
                     });
@@ -44,19 +44,19 @@ var server = http.createServer(function(req, res) {
                     });
                     break;
                 case '/character/createCharacter':
-                    con.query("INSERT INTO Characters (name, userid, race, class, age, gender, description) VALUES ('test', 1, 'test123', 'test123', 21, 'female', 'testing')", function(err,data) {
+                    con.query("INSERT INTO Characters (name, userid, race, class, age, gender, description) VALUES (?, ?, ?, ?, ?, ?,?)",['test', 1, 'test123', 'test123', 21, 'female', 'testing'], function(err,data) {
                         res.write(JSON.stringify(data));
                         res.end();
                     });
                     break;
                 case '/character/editCharacter':
-                    con.query("UPDATE Characters SET name = 'test123', race = 'test', class = 'test', age = 25, gender = 'male', description = 'testify' WHERE name = 'test'", function(err,data) {
+                    con.query("UPDATE Characters SET name = ?, race = ?, class = ?, age = ?, gender = ?, description = ? WHERE name = ?",['test123', 'test', 'test', 25, 'male', 'testify', 'test'], function(err,data) {
                         res.write(JSON.stringify(data));
                         res.end();
                     });
                     break;
                 case '/character/deleteCharacter':
-                    con.query("DELETE FROM Characters WHERE name = 'test123'", function(err,data) {
+                    con.query("DELETE FROM Characters WHERE name = '?'",['jaej'], function(err,data) {
                         res.write(JSON.stringify(data));
                         res.end();
                     });
